0a1886911bca5b149703fb192501de347f0a5bf7
Msabroe <57985286+Msabroe@users.noreply.github.com>
2020-05-13 11:27:04 +0200

    Klasser i guess

---
+++ Booking app/Booking app/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking_app.Model
+{
+    class User
+    {
+        public string UserMail { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
