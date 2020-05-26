b933ec46c51d56ef92c4cb6513657d43fd2528a6
Mike Sabroe Nielsen <mikesabroe@gmail.com>
2020-05-26 13:35:34 +0200

    UserInfo

--- Booking app/Booking app/Utility/Navigation.cs
+++ Booking app/Booking app/Utility/Navigation.cs
@@ -15,7 +15,7 @@ namespace Booking_app.Utility
         public static event Navigate navigateEvent;
         public static Type PageTarget { get; set; }
         public static string PageDeparture { get; set; }
-        public static Dictionary<string,Type> PageDictionary = new Dictionary<string, Type> {{"MainPage",typeof(Booking_app.View.MainPage) }, {"Login", typeof(Login)}, {"BookPage", typeof(BookPage)},{"CreateUser", typeof(CreateUser)}};
+        public static Dictionary<string,Type> PageDictionary = new Dictionary<string, Type> {{"MainPage",typeof(Booking_app.View.MainPage) }, {"Login", typeof(Login)}, {"BookPage", typeof(BookPage)},{"CreateUser", typeof(CreateUser)}, {"UserInfo", typeof(UserInfo)}};
 
         public static void NavigateToPage(string pageTarget, string pageDeparture)
         {
